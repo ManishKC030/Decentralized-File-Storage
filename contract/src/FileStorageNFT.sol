5f04ec6559bd411e0342477ac503b161c2e692a7
Manish Kc <manishkc2004@gmail.com>
2026-07-06 08:54:54 +0000

    feat: integrate Ownable for access control in FileStorageNFT contract

--- contract/src/FileStorageNFT.sol
+++ contract/src/FileStorageNFT.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.20;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract FileStorageNFT is ERC721 {
+contract FileStorageNFT is ERC721, Ownable {
 
     uint256 public tokenCounter;
 
     constructor()
         ERC721("FileStorageNFT", "FSNFT")
+        Ownable(msg.sender)
     {}
 
     error EmptyCID();
